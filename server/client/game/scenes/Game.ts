2eb35a8c643b9097536e259f7d6da662c6e704b5
Colin Doherty <colinjdoherty@outlook.com>
2025-12-24 16:26:50 -0800

    added ClientModel Manager

--- server/client/game/scenes/Game.ts
+++ server/client/game/scenes/Game.ts
@@ -1,11 +1,12 @@
 import { Scene } from 'phaser';
 import io, {type Socket} from 'socket.io-client'
 import {playerInfo} from '../../../common/SocketProtocols'
+import { ClientModelManager } from '../managers/ClientModelManager';
 
 export class Game extends Scene{
 
-    socket: typeof Socket | null = null;
-    playerGroup?:Phaser.GameObjects.Group
+    socket?: typeof Socket;
+    model?: ClientModelManager;
 
     constructor ()
     {
@@ -16,7 +17,6 @@ export class Game extends Scene{
         
     }
 
-    players?:Map<string, Phaser.GameObjects.Image>
 
     cursors?:Phaser.Types.Input.Keyboard.CursorKeys
     leftKeyPressed = false
@@ -26,38 +26,22 @@ export class Game extends Scene{
 
     create ()
     {
+        this.model = new ClientModelManager(this.add)
+
         this.socket = io();
-        console.log(this.socket)
-        this.playerGroup = this.add.group();
-        this.players = new Map()
 
         this.socket.on('gameState',  (players:playerInfo[]) => {
-            console.log("gameState")
             if(!this.socket)
-                throw new Error("invalid socket id")
-            players.forEach( (player)=>{
-                if (player.id === (this.socket as typeof Socket).id) {
-                    this.displayPlayers(player, 'ship');
-                } else {
-                    this.displayPlayers(player, 'otherPlayer');
-                }
-            });
+                throw new Error("no socket :(")
+            this.model?.addAllPlayers(this.add, players, this.socket?.id)
         });
 
         this.socket.on('newPlayer', (player:playerInfo)=>{
-            console.log("new player")
-            this.displayPlayers(player, 'otherPlayer');
+            this.model?.addPlayer(this.add, player, 'otherPlayer');
         });
-        this.socket.on('playerDisconnect',  (playerID:string)=>{
-            console.log(playerID)
-            if(!this.players)
-                throw new Error("players group not created")
-            this.players.forEach( (rep, id) => {
-                if(playerID == id){
-                    rep.destroy()
-                    this.players?.delete(id)
-                }
-            });
+        this.socket.on('playerDisconnect',  (id:string)=>{
+            console.log(id)
+            this.model?.removePlayer(id)
         });
 
         this.input.once('pointerdown', () => {
@@ -67,18 +51,7 @@ export class Game extends Scene{
         });
 
         this.socket.on('playerUpdates',  (players:playerInfo[])=>{
-            if(!this.players)
-                throw new Error("no players")
-            players.forEach( (info)=>{
-                if(!this.players)
-                        throw new Error("no players")
-                this.players.forEach(function (player, id) {
-                    if (info.id === id) {
-                        player.setRotation(info.rotation);
-                        player.setPosition(info.x, info.y);
-                    }
-                });
-            });
+            this.model?.updateAllPos(players)
         });
 
         if(!this.input.keyboard)
@@ -117,21 +90,5 @@ export class Game extends Scene{
 
         if (left !== this.leftKeyPressed || right !== this.rightKeyPressed || up !== this.upKeyPressed)
             this.socket.emit('playerInput', { left: this.leftKeyPressed , right: this.rightKeyPressed, up: this.upKeyPressed });
-    }
-
-    displayPlayers(playerI:playerInfo, sprite:string) {
-        const player = this.add.image(playerI.x, playerI.y, sprite)
-         .setOrigin(0.5, 0.5).setDisplaySize(53, 40);
-        
-        console.log(playerI.x, playerI.y, playerI.team)
-
-        if (playerI.team === 'blue') 
-            player.setTint(0x0000ff);
-        else 
-            player.setTint(0xff0000);
-        
-        if(!this.players)
-            throw new Error("player map not created")
-        this.players.set(playerI.id, player)
     }
 }
